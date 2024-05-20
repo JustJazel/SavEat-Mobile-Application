700469f9dfc6ef69204374d9f27f00821ec167fe
Jazel Equia <equiajazel@gmail.com>
2024-05-21 03:41:52 +0800

    attempt 9

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ccs.v6.project',
+  appId: 'ccs.v8.project',
   appName: 'Saveat',
   webDir: 'dist',
   server: {
